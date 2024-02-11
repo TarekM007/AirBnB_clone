c1acefcf530e70fd4d40a37f6438bcbfd411c4ca
TarekM007 <tarekmoustafa36@gmail.com>
2024-02-11 14:00:28 -0800

    6

--- console.py
+++ console.py
@@ -4,19 +4,27 @@ Module for console
 """
 
 import cmd
+
+
 class HBNBCommand(cmd.Cmd):
+    """
+    HBNBCommand console class
+    """
     prompt = "(hbnb)"
 
     def do_quit(self, args):
         """exit the program"""
         return True
+
     def help_quit(self, args):
         """documented line to help understand quit command"""
         print("Quit command to exit the program")
+
     def do_EOF(self, args):
         """exit the program"""
-        print("")
+        print()
         return True
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
