1e83211219423290f4d4ddde845dccea50a9c345
TarekM007 <tarekmoustafa36@gmail.com>
2024-02-11 10:50:26 -0800

    3

--- models/base_model.py
+++ models/base_model.py
@@ -1,64 +1,40 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 """
 BaseModel class that defines all common attributes/methods
 for other classes
-
 """
 
+import uuid
 from datetime import datetime
-from models import storage
-from uuid import uuid4
-
 
 class BaseModel:
+    """Base class for all models"""
 
-    """ BaseModel Class definition """
-
-    def __init__(self, *args, **kwargs):
-        """ Constructor """
-
-        for key, value in kwargs.items():
-            if key == "__class__":
-                continue
-
-            if (key == "created_at" or key == "updated_at"):
-                value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-
-            setattr(self, key, value)
-
-        if "id" not in kwargs.keys():
-            self.id = str(uuid4())
-
-        if "created_at" not in kwargs.keys():
-            self.created_at = datetime.now()
-
-        if "updated_at" not in kwargs.keys():
-            self.updated_at = datetime.now()
-
-        if len(kwargs) == 0:
-            storage.new(self)
-
-    def __str__(self):
-        """ Defines what should be printed for each instance of the class """
-        st = "[{:s}] ({:s}) {:s}"
-        st = st.format(self.__class__.__name__, self.id, str(self.__dict__))
-        return st
+    def __init__(self):
+        """Initialization of a Base instance."""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def save(self):
-        """
-        Update the Public Instance Attr updated_at with the current datetime
-        """
+        """Updates the public instance attribute updated_at
+        with the current datetime"""
         self.updated_at = datetime.now()
-        storage.save()
 
     def to_dict(self):
-        """
-        returns a dictionary containing all keys/values of __dict__
-        of the instance
-        """
-        dcopy = self.__dict__.copy()
-        dcopy["__class__"] = self.__class__.__name__
-        dcopy["created_at"] = self.created_at.isoformat()
-        dcopy["updated_at"] = self.updated_at.isoformat()
-        return dcopy
+        """Returns a dictionary that contains all
+        keys/values of the instance"""
+        updated_dict = self.__dict__.copy()
+        updated_dict["__class__"] = self.__class__.__name__
+        updated_dict["created_at"] = self.created_at.isoformat()
+        updated_dict["updated_at"] = self.updated_at.isoformat()
+
+        return updated_dict
+
+    def __str__(self):
+        """Returns a readable string representation
+        of BaseModel instances"""
+        class_name = self.__class__.__name__
+
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
