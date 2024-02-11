a4d5331c87c35b74b0994b8b8fa2309be3dde578
TarekM007 <tarekmoustafa36@gmail.com>
2024-02-11 09:39:24 -0800

    3

---
+++ models/base_model.py
@@ -0,0 +1,23 @@
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        updated_dict = self.__dict__.copy()
+        updated_dict["__class__"] = self.__class__.__name__
+        updated_dict["created_at"] = self.created_at.isoformat()
+        updated_dict["updated_at"] = self.updated_at.isoformat()
+
+        return updated_dict
