7dce47eca628c608b0bf6a801541efee4a0c764a
TarekM007 <tarekmoustafa36@gmail.com>
2024-02-18 09:32:15 -0800

    17

---
+++ tests/test_console.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Unittest module for the Console
+"""
+import unittest
+
+
+class TestInstantiation(unittest.TestCase):
+    """Instantiation test"""
+    def test_init(self):
+        """tests"""
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
